f70d2a44dcbfe33608dc48547f4adb02c929e196
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-11-24 12:44:04 +0530

    [Gyanendra] Add: A Program For Single Cast Delegate

---
+++ Delegates and Events/Delegates and Events/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Delegates_and_Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            operation obj = new operation(SinglecastDelegate.Addition); // Object Creation For Single Delegate
+            Console.WriteLine("Addition is: {0}", obj(10, 20));
+        }
+    }
+}
